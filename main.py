2d1a5f438dcdd87c53cc7187226aef76493caf2c
Fatih <95353768+Yokanisha@users.noreply.github.com>
2023-10-16 16:55:03 +0200

    commit macBook test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Player:
         elif self.player == "O":
             self.player = "X"
 
+
 class Board:
     def __init__(self):
         self.board = [" " for _ in range(9)]
@@ -16,35 +17,52 @@ class Board:
     def mark(self, number):
         self.board[number - 1] = self.symbol.player
 
-    def boardValid(self):
-        if "-" in self.board:
+    def moveValid(self, number):
+        if self.board[number - 1] == " ":
+            return True
+        else:
             return False
 
-    def moveValid(self):
-        pass
-
     def win(self):
-        pass
+        if self.board[0] == self.board[1] == self.board[2] and self.board[0] != " " or \
+                self.board[3] == self.board[4] == self.board[5] and self.board[3] != " " or \
+                self.board[6] == self.board[7] == self.board[8] and self.board[6] != " " or \
+                self.board[0] == self.board[3] == self.board[6] and self.board[0] != " " or \
+                self.board[1] == self.board[4] == self.board[7] and self.board[1] != " " or \
+                self.board[2] == self.board[5] == self.board[8] and self.board[2] != " " or \
+                self.board[0] == self.board[4] == self.board[8] and self.board[0] != " " or \
+                self.board[2] == self.board[4] == self.board[6] and self.board[2] != " ":
+
+            print(f"Player {self.symbol.player} win!")
+            return True
+
+        elif " " not in self.board:
+            # 5,1,8,2,3,7,4,6,9
+            print("It's a draw")
+            return True
 
     def __str__(self):
         return self.board[0] + ' | ' + self.board[1] + ' | ' + self.board[2] + ' | \n' + \
                self.board[3] + ' | ' + self.board[4] + ' | ' + self.board[5] + ' | \n' + \
-               self.board[6] + ' | ' + self.board[7] + ' | ' + self.board[8] + ' | '
+               self.board[6] + ' | ' + self.board[7] + ' | ' + self.board[8] + ' | \n'
 
 
 if __name__ == "__main__":
     board = Board()
 
-while(True):
-    playersInput = int(input("Gebe die Nummer ein: "))
-    board.mark(playersInput)
+while True:
+    while True:
+        playersInput = int(input("Gebe die Nummer ein: "))
+        if not board.moveValid(playersInput):
+            print("Invalid move. Try again.")
+        else:
+            break
 
+    board.mark(playersInput)
     print(board)
 
+    if board.win():
+        break
+
     board.symbol.changePlayer()
-
-
-
-
-
 
